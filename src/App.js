ae12cceca944e92adfc421bd247ef99ae1eb30c7
Asim <asaeed33@myseneca.ca>
2022-09-18 15:30:05 +0500

    setup homepage and added initial styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <h1>Welcome</h1>;
+  return (
+    <div className='grid-container'>
+      <header>
+        <a href='/'>Home Page</a>
+      </header>
+      <main>Product List</main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
 }
 
 export default App;
